8a2763b33bd92f2bc92fbb7424e7fb3794b026d8
vflopez <47512051+vflopez@users.noreply.github.com>
2021-03-31 16:59:00 -0400

    Getting candidate names

--- PyPoll.py
+++ PyPoll.py
@@ -14,15 +14,35 @@ file_to_load = os.path.join('Resources', 'election_results.csv')
 # Assign a variable to the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# 1. Initialize vote accumulator
+total_votes = 0
+# Initialize the candidate list
+candidate_options = []
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
      # Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
-    # Print the header row.
+    # Read header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Increment total_vote by 1 for each row in file
+    for row in file_reader:
+        # 2. Add to total vote count.
+        total_votes += 1
+
+        # Get the candidate name from each row
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+             candidate_options.append(candidate_name)
+
+# Print the candidate names
+print(candidate_options)
+
+# 3. Print the total votes.
+print(total_votes)
 
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
