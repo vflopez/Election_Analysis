36f62484d66dd493ee6fc184c6d356dd0d80715d
vflopez <47512051+vflopez@users.noreply.github.com>
2021-03-31 14:33:58 -0400

    Update Python_practice.py

--- Python_practice.py
+++ Python_practice.py
@@ -8,11 +8,18 @@
 #     # print(county + " county has " + str(voters) + " registered voters.")
 #     print(f"{county} county has {voters} registered voters!")
 
-candidate_votes = int(input("How many votes did the candidate get in the election? "))
-total_votes = int(input("What is the total number of votes in the election? "))
-message_to_candidate = (
-    f"You received {candidate_votes:,} number of votes. \n"
-    f"The total number of votes in the election was {total_votes:,}. \n"
-    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+# candidate_votes = int(input("How many votes did the candidate get in the election? "))
+# total_votes = int(input("What is the total number of votes in the election? "))
+# message_to_candidate = (
+#     f"You received {candidate_votes:,} number of votes. \n"
+#     f"The total number of votes in the election was {total_votes:,}. \n"
+#     f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
 
-print(message_to_candidate)
+# print(message_to_candidate)
+
+# Import the datetime class from the datetime module.
+import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
