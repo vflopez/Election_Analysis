ec515141b6e79fd4c459063e119d058d4ea78ca5
vflopez <47512051+vflopez@users.noreply.github.com>
2021-03-31 15:11:06 -0400

    Working on Analysis

--- PyPoll.py
+++ PyPoll.py
@@ -11,19 +11,23 @@ import os
 
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join('Resources', 'election_results.csv')
+# Assign a variable to the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
-     # To do: perform analysis.
-     print(election_data)
+     # Read the file object with the reader function
+    file_reader = csv.reader(election_data)
+    for row in file_reader:
+        print(row)
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+
 
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
 
-    # Write some data to the file.
-    txt_file.write("Hello World")
-
+     # Write three counties to the file.
+     txt_file.write('Counties in the Election\n')
+     txt_file.write('-------------------------\n')
+     txt_file.write("Arapahoe\nDenver\nJefferson")
