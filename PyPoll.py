d4ed6f6eec279ab9322d8ffe428ead040f90c752
vflopez <47512051+vflopez@users.noreply.github.com>
2021-03-31 14:16:49 -0400

    Create PyPoll.py

---
+++ PyPoll.py
@@ -0,0 +1,7 @@
+# The data we need to retreive.
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidata won
+# 5. The winner of the election based on popular vote
+
