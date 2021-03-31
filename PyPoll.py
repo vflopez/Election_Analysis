af6531273580ecc9faa607408f506c56231d20cf
vflopez <47512051+vflopez@users.noreply.github.com>
2021-03-31 14:59:18 -0400

    Starting to create output file

--- PyPoll.py
+++ PyPoll.py
@@ -5,3 +5,25 @@
 # 4. The total number of votes each candidata won
 # 5. The winner of the election based on popular vote
 
+# Load modules
+import csv
+import os
+
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join('Resources', 'election_results.csv')
+
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+
+     # To do: perform analysis.
+     print(election_data)
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Hello World")
+
